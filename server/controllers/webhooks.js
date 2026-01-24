e6bf8837c2d77c963d5f195079072c4fa714fcdf
Manthan Singla <manthan29singla@gmail.com>
2026-01-24 19:38:53 +0530

    Changes Occures in webhooks.js

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -8,7 +8,7 @@ const User= require('../models/User');
 const clerkWebhooks= async (req,res)=>{
     try{
         const whook= new Webhook(process.env.CLERK_WEBHOOK_SECRET);
-        await whook.verify(JSON.stringify(req.body),{
+        const event= await whook.verify(JSON.stringify(req.body),{
             "svix-id": req.headers["svix-id"],
             "svix-timestamp": req.headers["svix-timestamp"],
             "svix-signature": req.headers["svix-signature"],
