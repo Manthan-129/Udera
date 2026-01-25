23da75c049899e7b77255f9ecb4959fd405f66d1
Manthan Singla <manthan29singla@gmail.com>
2026-01-25 12:46:50 +0530

    Server

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const PORT= process.env.PORT || 5000;
 
 // Database Connection
 const {connectDB}= require('./config/db');
-await connectDB();
+connectDB();
 const {cloudinaryConnect}= require('./config/cloudinary');
 cloudinaryConnect();
 
