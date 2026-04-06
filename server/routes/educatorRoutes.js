96df7c89e960e246c341d40fa376435b3952b93e
Manthan Singla <manthan29singla@gmail.com>
2026-04-06 19:18:11 +0530

    Add the Delete function for the Course Instructor

--- server/routes/educatorRoutes.js
+++ server/routes/educatorRoutes.js
@@ -1,12 +1,13 @@
 const express= require('express');
 const educatorRouter= express.Router();
 const {authMiddleware, protectEducator}= require('../middlewares/authMiddleware.js');
-const {updateRoleToEducator, addCourse, getEducatorCourses, getEnrolledStudentsData, educatorDashboardData }= require('../controllers/educatorController.js');
+const {updateRoleToEducator, addCourse,deleteCourse, getEducatorCourses, getEnrolledStudentsData, educatorDashboardData }= require('../controllers/educatorController.js');
 const upload = require('../middlewares/multer').upload;
 
 // Route to update user role to educator
 educatorRouter.post('/update-role', authMiddleware, updateRoleToEducator);
 educatorRouter.post('/add-course', authMiddleware, protectEducator, upload.single('image'), addCourse);
+educatorRouter.delete('/courses/:CourseId', authMiddleware, protectEducator, deleteCourse);
 educatorRouter.get('/courses', authMiddleware, protectEducator, getEducatorCourses);
 educatorRouter.get('/dashboard', authMiddleware, protectEducator, educatorDashboardData);
 educatorRouter.get('/enrolled-students', authMiddleware, protectEducator, getEnrolledStudentsData);
